4ee411a3d71a8658734e7ecb09aa3efe2bdb4e1c
Ferry Huberts <ferry.huberts@pelagic.nl>
2015-11-11 12:48:22 +0100

    main: move CLI related function into their own file
    And refactor to not invoke olsr_exit
    
    Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

--- src/main.c
+++ src/main.c
@@ -66,6 +66,7 @@
 #include "olsr_random.h"
 #include "pid_file.h"
 #include "lock_file.h"
+#include "cli.h"
 
 #ifdef __linux__
 #include <linux/types.h>
@@ -80,7 +81,6 @@
 #define olsr_shutdown(x) SignalHandler(x)
 #define close(x) closesocket(x)
 int __stdcall SignalHandler(unsigned long signo) __attribute__ ((noreturn));
-void ListInterfaces(void);
 void DisableIcmpRedirects(void);
 #else /* _WIN32 */
 static void olsr_shutdown(int) __attribute__ ((noreturn));
@@ -94,13 +94,7 @@ struct timer_entry * heartBeatTimer = NULL;
 
 void olsr_reconfigure(int signo) __attribute__ ((noreturn));
 
-static void print_version(void);
-static void print_usage(bool error);
-
 static int set_default_ifcnfs(struct olsr_if *, struct if_config_options *);
-
-static int olsr_process_arguments(int, char *[], struct olsrd_config *,
-    struct if_config_options *);
 
 static char **olsr_argv = NULL;
 
@@ -178,6 +172,9 @@ int main(int argc, char *argv[]) {
 
   /* process arguments */
   {
+    char * error = NULL;
+    int exitCode = EXIT_FAILURE;
+
     /* get the default interface config */
     struct if_config_options *default_ifcnf = get_default_if_config();
     if (!default_ifcnf) {
@@ -185,10 +182,10 @@ int main(int argc, char *argv[]) {
     }
 
     /* Process olsrd options */
-    if (olsr_process_arguments(argcLocal, argv, olsr_cnf, default_ifcnf) < 0) {
-      print_usage(true);
+    exitCode = olsr_process_arguments(argcLocal, argv, olsr_cnf, default_ifcnf, &error);
+    if (exitCode != 0) {
       free(default_ifcnf);
-      olsr_exit(NULL, EXIT_FAILURE);
+      olsr_exit(error, EXIT_FAILURE);
     }
 
     /* Set configuration for command-line specified interfaces */
@@ -664,30 +661,6 @@ static void olsr_shutdown(int signo __attribute__ ((unused)))
   exit(exit_value);
 }
 
-static void print_version(void) {
-  printf("\n *** %s ***\n Build date: %s on %s\n http://www.olsr.org\n\n",
-      olsrd_version, build_date, build_host);
-}
-
-/**
- * Print the command line usage
- */
-static void print_usage(bool error) {
-  fprintf(
-      stderr,
-        "%s"
-        "usage: olsrd [-f <configfile>] [ -i interface1 interface2 ... ]\n"
-        "  [-d <debug_level>] [-ipv6] [-multi <IPv6 multicast address>]\n"
-        "  [-lql <LQ level>] [-lqw <LQ winsize>] [-lqnt <nat threshold>]\n"
-        "  [-bcast <broadcastaddr>] [-ipc] [-delgw]\n"
-        "  [-hint <hello interval (secs)>] [-tcint <tc interval (secs)>]\n"
-        "  [-midint <mid interval (secs)>] [-hnaint <hna interval (secs)>]\n"
-        "  [-T <Polling Rate (secs)>] [-nofork] [-hemu <ip_address>]\n"
-        "  [-lql <LQ level>] [-lqa <LQ aging factor>]\n"
-        "  [-pidfile <pid file>]\n",
-        error ? "Error in command line parameters!\n" : "");
-}
-
 /**
  * Sets the provided configuration on all unconfigured
  * interfaces
@@ -708,294 +681,6 @@ int set_default_ifcnfs(struct olsr_if *ifs, struct if_config_options *cnf) {
     ifs = ifs->next;
   }
   return changes;
-}
-
-#define NEXT_ARG do { argv++;argc--; } while (0)
-#define CHECK_ARGC do { if(!argc) { \
-     char buf[1024]; \
-     argv--; \
-     snprintf(buf, sizeof(buf), "%s: You must provide a parameter when using the %s switch", __func__, *argv); \
-     olsr_exit(buf, EXIT_FAILURE); \
-     } } while (0)
-
-/**
- * Process command line arguments passed to olsrd
- *
- */
-static int olsr_process_arguments(int argc, char *argv[],
-    struct olsrd_config *cnf, struct if_config_options *ifcnf) {
-  while (argc > 1) {
-    NEXT_ARG;
-#ifdef _WIN32
-    /*
-     *Interface list
-     */
-    if (strcmp(*argv, "-int") == 0) {
-      ListInterfaces();
-      olsr_exit(NULL, 0);
-    }
-#endif /* _WIN32 */
-
-    /*
-     *Configfilename
-     */
-    if (strcmp(*argv, "-f") == 0) {
-      char buf2[1024];
-      snprintf(buf2, sizeof(buf2), "%s: Configfilename must ALWAYS be first argument", __func__);
-      olsr_exit(buf2, EXIT_FAILURE);
-    }
-
-    /*
-     *Use IP version 6
-     */
-    if (strcmp(*argv, "-ipv6") == 0) {
-      cnf->ip_version = AF_INET6;
-      continue;
-    }
-
-    /*
-     *Broadcast address
-     */
-    if (strcmp(*argv, "-bcast") == 0) {
-      struct in_addr in;
-      NEXT_ARG;
-      CHECK_ARGC;
-
-      if (inet_pton(AF_INET, *argv, &in) == 0) {
-        printf("Invalid broadcast address! %s\nSkipping it!\n", *argv);
-        continue;
-      }
-      memcpy(&ifcnf->ipv4_multicast.v4, &in.s_addr, sizeof(ifcnf->ipv4_multicast.v4));
-      continue;
-    }
-
-    /*
-     * Set LQ level
-     */
-    if (strcmp(*argv, "-lql") == 0) {
-      int tmp_lq_level;
-      NEXT_ARG;
-      CHECK_ARGC;
-
-      /* Sanity checking is done later */
-      sscanf(*argv, "%d", &tmp_lq_level);
-      olsr_cnf->lq_level = tmp_lq_level;
-      continue;
-    }
-
-    /*
-     * Set LQ winsize
-     */
-    if (strcmp(*argv, "-lqa") == 0) {
-      float tmp_lq_aging;
-      NEXT_ARG;
-      CHECK_ARGC;
-
-      sscanf(*argv, "%f", &tmp_lq_aging);
-
-      if (tmp_lq_aging < (float)MIN_LQ_AGING || tmp_lq_aging > (float)MAX_LQ_AGING) {
-        char buf[1024];
-        snprintf(buf, sizeof(buf), "%s: LQ aging factor %f not allowed. Range [%f-%f]", __func__,
-            (double)tmp_lq_aging, (double)MIN_LQ_AGING, (double)MAX_LQ_AGING);
-        olsr_exit(buf, EXIT_FAILURE);
-      }
-      olsr_cnf->lq_aging = tmp_lq_aging;
-      continue;
-    }
-
-    /*
-     * Set NAT threshold
-     */
-    if (strcmp(*argv, "-lqnt") == 0) {
-      float tmp_lq_nat_thresh;
-      NEXT_ARG;
-      CHECK_ARGC;
-
-      sscanf(*argv, "%f", &tmp_lq_nat_thresh);
-
-      if (tmp_lq_nat_thresh < 0.1f || tmp_lq_nat_thresh > 1.0f) {
-        char buf[1024];
-        snprintf(buf, sizeof(buf), "%s: NAT threshold %f not allowed. Range [%f-%f]", __func__,
-        		(double)tmp_lq_nat_thresh, (double)0.1, (double)1.0);
-        olsr_exit(buf, EXIT_FAILURE);
-      }
-      olsr_cnf->lq_nat_thresh = tmp_lq_nat_thresh;
-      continue;
-    }
-
-    /*
-     * Enable additional debugging information to be logged.
-     */
-    if (strcmp(*argv, "-d") == 0) {
-      NEXT_ARG;
-      CHECK_ARGC;
-
-      sscanf(*argv, "%d", &cnf->debug_level);
-      continue;
-    }
-
-    /*
-     * Interfaces to be used by olsrd.
-     */
-    if (strcmp(*argv, "-i") == 0) {
-      NEXT_ARG;
-      CHECK_ARGC;
-
-      if (*argv[0] == '-') {
-        char buf[1024];
-        snprintf(buf, sizeof(buf), "%s: You must provide an interface label", __func__);
-        olsr_exit(buf, EXIT_FAILURE);
-      }
-      printf("Queuing if %s\n", *argv);
-      olsr_create_olsrif(*argv, false);
-
-      while ((argc - 1) && (argv[1][0] != '-')) {
-        NEXT_ARG;
-        printf("Queuing if %s\n", *argv);
-        olsr_create_olsrif(*argv, false);
-      }
-
-      continue;
-    }
-    /*
-     * Set the hello interval to be used by olsrd.
-     *
-     */
-    if (strcmp(*argv, "-hint") == 0) {
-      NEXT_ARG;
-      CHECK_ARGC;
-      sscanf(*argv, "%f", &ifcnf->hello_params.emission_interval);
-      ifcnf->hello_params.validity_time = ifcnf->hello_params.emission_interval
-          * 3;
-      continue;
-    }
-
-    /*
-     * Set the HNA interval to be used by olsrd.
-     *
-     */
-    if (strcmp(*argv, "-hnaint") == 0) {
-      NEXT_ARG;
-      CHECK_ARGC;
-      sscanf(*argv, "%f", &ifcnf->hna_params.emission_interval);
-      ifcnf->hna_params.validity_time = ifcnf->hna_params.emission_interval * 3;
-      continue;
-    }
-
-    /*
-     * Set the MID interval to be used by olsrd.
-     *
-     */
-    if (strcmp(*argv, "-midint") == 0) {
-      NEXT_ARG;
-      CHECK_ARGC;
-      sscanf(*argv, "%f", &ifcnf->mid_params.emission_interval);
-      ifcnf->mid_params.validity_time = ifcnf->mid_params.emission_interval * 3;
-      continue;
-    }
-
-    /*
-     * Set the tc interval to be used by olsrd.
-     *
-     */
-    if (strcmp(*argv, "-tcint") == 0) {
-      NEXT_ARG;
-      CHECK_ARGC;
-      sscanf(*argv, "%f", &ifcnf->tc_params.emission_interval);
-      ifcnf->tc_params.validity_time = ifcnf->tc_params.emission_interval * 3;
-      continue;
-    }
-
-    /*
-     * Set the polling interval to be used by olsrd.
-     */
-    if (strcmp(*argv, "-T") == 0) {
-      NEXT_ARG;
-      CHECK_ARGC;
-      sscanf(*argv, "%f", &cnf->pollrate);
-      continue;
-    }
-
-    /*
-     * Should we set up and send on a IPC socket for the front-end?
-     */
-    if (strcmp(*argv, "-ipc") == 0) {
-      cnf->ipc_connections = 1;
-      continue;
-    }
-
-    /*
-     * IPv6 multicast addr
-     */
-    if (strcmp(*argv, "-multi") == 0) {
-      struct in6_addr in6;
-      NEXT_ARG;
-      CHECK_ARGC;
-      if (inet_pton(AF_INET6, *argv, &in6) <= 0) {
-        char buf[1024];
-        snprintf(buf, sizeof(buf), "Failed converting IP address %s", *argv);
-        olsr_exit(buf, EXIT_FAILURE);
-      }
-
-      memcpy(&ifcnf->ipv6_multicast, &in6, sizeof(struct in6_addr));
-
-      continue;
-    }
-
-    /*
-     * Host emulation
-     */
-    if (strcmp(*argv, "-hemu") == 0) {
-      struct in_addr in;
-      struct olsr_if *ifa;
-
-      NEXT_ARG;
-      CHECK_ARGC;
-      if (inet_pton(AF_INET, *argv, &in) <= 0) {
-        char buf[1024];
-        snprintf(buf, sizeof(buf), "Failed converting IP address %s", *argv);
-        olsr_exit(buf, EXIT_FAILURE);
-      }
-      /* Add hemu interface */
-
-      ifa = olsr_create_olsrif("hcif01", true);
-
-      if (!ifa)
-        continue;
-
-      ifa->cnf = get_default_if_config();
-      ifa->host_emul = true;
-      memset(&ifa->hemu_ip, 0, sizeof(ifa->hemu_ip));
-      memcpy(&ifa->hemu_ip, &in, sizeof(in));
-      cnf->host_emul = true;
-
-      continue;
-    }
-
-    /*
-     * Delete possible default GWs
-     */
-    if (strcmp(*argv, "-delgw") == 0) {
-      olsr_cnf->del_gws = true;
-      continue;
-    }
-
-    if (strcmp(*argv, "-nofork") == 0) {
-      cnf->no_fork = true;
-      continue;
-    }
-
-    if (strcmp(*argv, "-pidfile") == 0) {
-      NEXT_ARG;
-      CHECK_ARGC;
-
-      cnf->pidfile = *argv;
-      continue;
-    }
-
-    return -1;
-  }
-  return 0;
 }
 
 /*
