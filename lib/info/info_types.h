omokeri/nd_olsrd

commit 9d1ac7e98230b43833e4cc5ebe1cae033e9d3627

info: lazily initialise the cache
Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

lib/info/info_types.h
@@ -129,6 +129,7 @@ typedef struct {
 } info_plugin_functions_t;
 
 struct info_cache_entry_t {
+    bool initialised;
     long long timestamp;
     struct autobuf buf;
 };

lib/info/olsrd_info.c
@@ -188,12 +188,22 @@ static void info_plugin_cache_init(bool init) {
     }
 
     if (init) {
-      entry->timestamp = 0;
-      abuf_init(&entry->buf, AUTOBUFCHUNK);
+      entry->initialised = false;
     } else {
-      abuf_free(&entry->buf);
+      if (entry->initialised) {
+        abuf_free(&entry->buf);
+        entry->initialised = false;
+      }
       entry->timestamp = 0;
     }
+  }
+}
+
+static INLINE void info_plugin_cache_init_entry(struct info_cache_entry_t * entry) {
+  if (!entry->initialised) {
+    entry->timestamp = 0;
+    abuf_init(&entry->buf, AUTOBUFCHUNK);
+    entry->initialised = true;
   }
 }
 
@@ -380,6 +390,7 @@ static void send_info_from_table(struct autobuf *abuf, unsigned int send_what, S
         } else {
           long long now = olsr_times();
           long long age = abs(now - cache_entry->timestamp);
+          info_plugin_cache_init_entry(cache_entry);
           if (!cache_entry->timestamp || (age >= cache_timeout)) {
             /* cache is never used before or cache is too old */
             cache_entry->buf.buf[0] = '\0';

commit 195778729e4ac3b37de8d16f76cac67d5c006575

info: better initialisation of the cache structures
Commit 9d1ac7e broke this

Signed-off-by: Ferry Huberts <ferry.huberts@pelagic.nl>

lib/info/info_types.h
@@ -152,7 +152,6 @@ typedef struct {
 } info_plugin_functions_t;
 
 struct info_cache_entry_t {
-    bool initialised;
     long long timestamp;
     struct autobuf buf;
 };

lib/info/olsrd_info.c
@@ -205,22 +205,19 @@ static void info_plugin_cache_init(bool init) {
     }
 
     if (init) {
-      entry->initialised = false;
+      entry->timestamp = 0;
+      abuf_init(&entry->buf, 0);
     } else {
-      if (entry->initialised) {
-        abuf_free(&entry->buf);
-        entry->initialised = false;
-      }
+      abuf_free(&entry->buf);
       entry->timestamp = 0;
     }
   }
 }
 
 static INLINE void info_plugin_cache_init_entry(struct info_cache_entry_t * entry) {
-  if (!entry->initialised) {
+  if (!entry->buf.buf) {
     entry->timestamp = 0;
     abuf_init(&entry->buf, AUTOBUFCHUNK);
-    entry->initialised = true;
   }
 }
 
@@ -424,9 +421,13 @@ static void send_info_from_table(struct autobuf *abuf, unsigned int send_what, S
         if (!cache_entry) {
             func(abuf);
         } else {
-          long long now = olsr_times();
-          long long age = abs(now - cache_entry->timestamp);
+          long long now;
+          long long age;
+
           info_plugin_cache_init_entry(cache_entry);
+
+          now = olsr_times();
+          age = abs(now - cache_entry->timestamp);
           if (!cache_entry->timestamp || (age >= cache_timeout)) {
             /* cache is never used before or cache is too old */
             cache_entry->buf.buf[0] = '\0';