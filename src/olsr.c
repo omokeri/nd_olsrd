62953a538aeaeeb01258a9179466e804190f4ab3
omokeri <omoyakhire@gmail.com>
2020-07-15 11:51:33 +0900

    Update olsr.c

--- src/olsr.c
+++ src/olsr.c
@@ -644,26 +644,6 @@ ReceiveMsgFromOLSR(unsigned char *encapsulationUdpData, int len)
   return ncount;
 }                               
 
-int
-apm_read(struct olsr_apm_info *ApmInfo)
-{
-#if !defined WINCE
-  SYSTEM_POWER_STATUS PowerStat;
-
-  memset(ApmInfo, 0, sizeof(struct olsr_apm_info));
-
-  if (!GetSystemPowerStatus(&PowerStat))
-    return 0;
-
-  ApmInfo->ac_line_status = (PowerStat.ACLineStatus == 1) ? OLSR_AC_POWERED : OLSR_BATTERY_POWERED;
-
-  ApmInfo->battery_percentage = (PowerStat.BatteryLifePercent <= 100) ? PowerStat.BatteryLifePercent : 0;
-
-  return 1;
-#else /* !defined WINCE */
-  return 0;
-#endif /* !defined WINCE */
-}
 
 uint8_t
 olsr_calculate_willingness(void)
